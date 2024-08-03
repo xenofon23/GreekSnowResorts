f47fe16c7fa97cca675ad5d65cc41010d890d928
xenofon <pantsoscen23@gmail.com>
2024-08-03 12:19:09 +0300

    fix migrations

--- GreekSnowResortsApi/database/migrations/2024_07_15_110026_rename_booking_trable_to_bookings.php
+++ GreekSnowResortsApi/database/migrations/2024_07_15_110026_rename_booking_trable_to_bookings.php
@@ -11,8 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::rename('bookings_trable', 'bookings');
-
+        if (Schema::hasTable('bookings')) {
+            Schema::table('bookings', function (Blueprint $table) {
+                // Change the type of the renamed column
+                $table->time('order_time')->change();
+            });
+        } else {
+            // Handle the case where the table does not exist
+            // You might want to throw an exception or log this
+            throw new Exception('Table bookings does not exist');
+        }
     }
 
     /**
@@ -20,8 +28,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('bookings', function (Blueprint $table) {
-            //
-        });
+        if (Schema::hasTable('bookings')) {
+            Schema::rename('bookings', 'bookings_trable');
+        } else {
+            // Handle the case where the table does not exist
+            // You might want to throw an exception or log this
+            throw new Exception('Table bookings does not exist');
+        }
     }
 };
--- GreekSnowResortsApi/database/migrations/2024_07_15_110929_change_type_column_to_bookings_table.php
+++ GreekSnowResortsApi/database/migrations/2024_07_15_110929_change_type_column_to_bookings_table.php
@@ -11,16 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (Schema::hasTable('bookings')) {
-            Schema::table('bookings', function (Blueprint $table) {
-                // Change the type of the renamed column
-                $table->time('order_time')->change();
-            });
-        } else {
-            // Handle the case where the table does not exist
-            // You might want to throw an exception or log this
-            throw new Exception('Table bookings does not exist');
-        }
+
+        Schema::table('bookings', function (Blueprint $table) {
+            // Change the type of the renamed column
+            $table->time('order_time')->change();
+        });
     }
 
     /**
@@ -28,13 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        if (Schema::hasTable('bookings')) {
-            Schema::rename('bookings', 'bookings_trable');
-        } else {
-            // Handle the case where the table does not exist
-            // You might want to throw an exception or log this
-            throw new Exception('Table bookings does not exist');
-        }
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
     }
 };
-//
