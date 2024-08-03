0dd84c6d483a514d6c51cde7843f69db8db459a1
xenofon <pantsoscen23@gmail.com>
2024-08-03 12:16:54 +0300

    fix migrations

--- GreekSnowResortsApi/database/migrations/2024_07_15_110929_change_type_column_to_bookings_table.php
+++ GreekSnowResortsApi/database/migrations/2024_07_15_110929_change_type_column_to_bookings_table.php
@@ -11,11 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-
-        Schema::table('bookings', function (Blueprint $table) {
-            // Change the type of the renamed column
-            $table->time('order_time')->change();
-        });
+        if (Schema::hasTable('bookings')) {
+            Schema::table('bookings', function (Blueprint $table) {
+                // Change the type of the renamed column
+                $table->time('order_time')->change();
+            });
+        } else {
+            // Handle the case where the table does not exist
+            // You might want to throw an exception or log this
+            throw new Exception('Table bookings does not exist');
+        }
     }
 
     /**
@@ -23,7 +28,13 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::rename('bookings', 'bookings_trable');
+        if (Schema::hasTable('bookings')) {
+            Schema::rename('bookings', 'bookings_trable');
+        } else {
+            // Handle the case where the table does not exist
+            // You might want to throw an exception or log this
+            throw new Exception('Table bookings does not exist');
+        }
     }
 };
 //
