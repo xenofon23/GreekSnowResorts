b1c117245d0efacf539626ae9489cab27ece7698
xenofon <pantsoscen23@gmail.com>
2024-08-03 12:21:55 +0300

    fix migrations

--- GreekSnowResortsApi/database/migrations/2024_07_15_101641_create_bookings_trable.php
+++ GreekSnowResortsApi/database/migrations/2024_07_15_101641_create_bookings_trable.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('bookings_trable');
+//        Schema::dropIfExists('bookings_trable');
     }
 };
