4085c996dbc75d9d99e2cd1c7330ca36aae484e6
xenofon <pantsoscen23@gmail.com>
2024-08-22 21:49:19 +0300

    add https

--- GreekSnowResortsApi/app/Providers/AppServiceProvider.php
+++ GreekSnowResortsApi/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS if the app is not running locally
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
