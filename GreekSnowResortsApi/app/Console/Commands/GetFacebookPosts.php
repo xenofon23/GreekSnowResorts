e5e9a77382b3b332d811a74b7079e5b419531e99
xpantsos <xpantsos@lancom.gr>
2025-01-12 17:43:21 +0200

    fix

--- GreekSnowResortsApi/app/Console/Commands/GetFacebookPosts.php
+++ GreekSnowResortsApi/app/Console/Commands/GetFacebookPosts.php
@@ -42,7 +42,7 @@ class GetFacebookPosts extends Command
             ->values()
             ->toArray();
 
-        $browserFactory = new BrowserFactory('chromium');
+        $browserFactory = new BrowserFactory();
         $posts=[];
         $browser = $browserFactory->createBrowser();
         foreach ($snowResortData as $item) {
