61f01b632bdbcfcd71b690e3e98b9de451320deb
xpantsos <xpantsos@lancom.gr>
2025-01-17 20:03:18 +0200

    add Limit

--- GreekSnowResortsApi/app/Providers/RouteServiceProvider.php
+++ GreekSnowResortsApi/app/Providers/RouteServiceProvider.php
@@ -28,13 +28,17 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+        RateLimiter::for('proxy', function (Request $request) {
+            return Limit::perMinute(100)->by($request->ip());
+        });
+
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+//            Route::middleware('web')
+//                ->group(base_path('routes/web.php'));
         });
     }
 }
